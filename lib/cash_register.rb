014d90709e71b0eae48914a2c38ec10543fb65aa
Rigz777 <rigzgoodmusic@gmail.com>
2015-12-01 04:33:15 -0500

    refactored

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,47 +1,36 @@
 require 'pry'
 class CashRegister
-
-  attr_accessor :total, :discount, :items, :transactions
-
+  attr_accessor :total, :discount
+  
   def initialize(discount = 0)
     @total = 0
     @discount = discount
-    @items = []
-    @transactions = []
+    @item = {}
+    @bag = []
+    @transaction = []
   end
 
   def add_item(title, price, quantity = 1)
-    quantity.times do
-      @items << title
-    end
-    hash = {}
-    new_price = price
-    hash[title] = price
-    hash.each do |k,v|
-      @transactions << v
-    end
-    
-    if quantity > 1
-      new_price = (quantity * price)
-    end
-    @total += new_price
+    @item[title] = price
+    @total += quantity * price
+    quantity.times{@bag << title}
+    @item.each {|thing, cost| @transaction << cost}
   end
 
   def apply_discount
     if discount != 0
-      new_price = (discount.to_f / 100) * total
-      @total -= new_price
-      return "After the discount, the total comes to $#{@total.to_i}."
+      @total -= (@discount.to_f / 100) * @total
+      "After the discount, the total comes to $#{@total.to_int}."
     else
-      return "There is no discount to apply."
+      "There is no discount to apply."
     end
   end
 
   def items
-    @items
+    @bag
   end
 
   def void_last_transaction
-    @total -= @transactions.pop 
+    @total -= @transaction.pop
   end
 end
